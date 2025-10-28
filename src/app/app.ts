7a7ee88d4255243a54e43014e9248aa432cff50d
J Celeste <jceleste1@gmail.com>
2025-10-27 21:50:28 -0300

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule}  from '@angular/material/toolbar';
+import { Footer } from './components/template/footer/footer';
+import { Header } from './components/template/header/header';
+import { Nav } from './components/template/nav/nav';
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{ title() }}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [RouterOutlet,Header,Footer,Nav],
+  templateUrl: 'app.component.html',
 })
 export class App {
   protected readonly title = signal('transf-agenda-finan-web');
